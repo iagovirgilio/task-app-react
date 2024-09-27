edac8c72c76f422a586dac83a9cb0b9906e19949
Iago Virgílio <iagovirgilio@gmail.com>
2024-09-27 14:24:00 -0300

    Adicionar navegação

--- src/App.js
+++ src/App.js
@@ -1,21 +1,59 @@
 import React from 'react';
-import Header from './components/Header/Header';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import TaskForm from './components/TaskForm/TaskForm';
 import TaskList from './components/TaskList/TaskList';
 import Loading from './components/Loading/Loading';
+import Navigation from './components/Navigation/Navigation';
 import { useTasks } from './hooks/useTasks';
-
 import './App.css';
+
+// Função que retorna o título baseado na rota
+const Header = () => {
+  const location = useLocation();
+
+  let headerTitle;
+  switch (location.pathname) {
+    case '/':
+      headerTitle = 'Bem-vindo ao Task-App!';
+      break;
+    case '/tasks':
+      headerTitle = 'Gerenciador de Tarefas';
+      break;
+    case '/about':
+      headerTitle = 'Sobre o Projeto';
+      break;
+    default:
+      headerTitle = 'Task-App';
+  }
+
+  return <h1>{headerTitle}</h1>;
+};
 
 function App() {
   const { tasks, loading, addTask, editTask, removeTask } = useTasks();
 
   return (
-    <div className="container">
-      <Header>Gerenciador de Tarefas</Header>
-      <TaskForm addTask={addTask} />
-      {loading ? <Loading /> : <TaskList tasks={tasks} editTask={editTask} removeTask={removeTask} />}
-    </div>
+    <Router>
+      <div className="container">
+        {/* Adicionando a navegação */}
+        <Navigation />
+        
+        {/* Header dinâmico com base na rota */}
+        <Header />
+        
+        {/* Configurando as rotas */}
+        <Routes>
+          <Route path="/" element={<h2>Home Page: Você pode navegar entre as páginas</h2>} />
+          <Route path="/tasks" element={
+            <>
+              <TaskForm addTask={addTask} />
+              {loading ? <Loading /> : <TaskList tasks={tasks} editTask={editTask} removeTask={removeTask} />}
+            </>
+          } />
+          <Route path="/about" element={<h2>Esta aplicação foi desenvolvida para gerenciar suas tarefas de forma eficiente.</h2>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,24 @@
+// Navigation.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navigation.css';
+
+function Navigation() {
+  return (
+    <nav className="navigation">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/tasks">Tarefas</Link>
+        </li>
+        <li>
+          <Link to="/about">Sobre</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
